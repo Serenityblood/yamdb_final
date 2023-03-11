9fd6cf7b62687cc06931b70ea5b776b040dfc46e
serenityblood <miracle_5@bk.ru>
2023-03-11 19:15:04 +0300

    except ssl

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = [os.getenv('HOSTS', default='*')]
 
@@ -60,10 +60,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
+if os.getenv('SQLITE_BOOL'):
+    ENGINE = 'django.db.backends.sqlite3'
+    NAME = os.path.join(BASE_DIR, 'db.sqlite3')
+else:
+    ENGINE = os.getenv('DB_ENGINE', default='django.db.backends.postgresql')
+    NAME = os.getenv('DB_NAME')
+
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'),
+        'ENGINE': ENGINE,
+        'NAME': NAME,
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
