8f8e15b37ada37df30c7ad3eaa6cd7fdb470ab86
serenityblood <miracle_5@bk.ru>
2023-03-11 16:19:24 +0300

    settings git push

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,17 +1,13 @@
 import os
 from datetime import timedelta
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.getenv('HOSTS', default='*')]
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
